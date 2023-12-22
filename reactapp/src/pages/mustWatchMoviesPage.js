bb33ea24853bd4fadf383803106bf95118d61589
Yiming Yu <2693604213@qq.com>
2023-12-22 22:59:58 +0000

    authentication

--- reactapp/src/pages/mustWatchMoviesPage.js
+++ reactapp/src/pages/mustWatchMoviesPage.js
@@ -4,13 +4,11 @@ import { MoviesContext } from "../contexts/moviesContext";
 import { useQueries } from "react-query";
 import { getMovie } from "../api/tmdb-api";
 import Spinner from '../components/spinner'
-import RemoveFromMustWatches from 
-'../components/cardIcons/removeFromMustWatches.js'
+import RemoveFromMustWatches from '../components/cardIcons/removeFromMustWatches.js'
 
 const MustWatchMoviesPage = () => {
   const {mustWatches: movieIds } = useContext(MoviesContext);
 
-  // Create an array of queries and run in parallel.
   const mustWatchMovieQueries = useQueries(
     movieIds.map((movieId) => {
       return {
@@ -20,7 +18,6 @@ const MustWatchMoviesPage = () => {
     })
   );
 
-  // Check if any of the parallel queries is still loading.
   const isLoading = mustWatchMovieQueries.find((m) => m.isLoading === 
 true);
 
